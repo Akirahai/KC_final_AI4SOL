03a61671a33cd33a809f4a6fc2214e4d2851e5d2
Akirahai <lehaivin03@gmail.com>
2024-08-16 10:06:24 +0800

    'sh'

--- scripts/21_08_preds_deliver.sh
+++ scripts/21_08_preds_deliver.sh
@@ -13,6 +13,6 @@ seeds=(5 10 15 20 42)
 # Loop through each model and seed
 for model in "${models[@]}"; do
   for seed in "${seeds[@]}"; do
-    python main.py --use-gpu --gpus 0 1 2 3 --phase test --batch-size 16 --lr 0.0001 --epochs 70 --model "result/21_08_deliver/seed_$seed/$model" --seed $seed --experiment 21_08_deliver --top-k 5
+    python main.py --use-gpu --gpus 0 1 2 3 --phase test --batch-size 16 --lr 0.0001 --epochs 70 --model "result/21_08_deliver/seed_$seed/$model" --seed $seed --experiment 21_08_deliver_test --top-k 5
   done
 done
