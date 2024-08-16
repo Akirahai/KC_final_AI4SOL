ab2e7c78a8036ac7ee5b4a2200a46be6235ac28e
Akirahai <lehaivin03@gmail.com>
2024-08-16 12:38:59 +0800

    'epoch'

--- scripts/21_08_exp.sh
+++ scripts/21_08_exp.sh
@@ -5,14 +5,14 @@ models=(
   "google-bert/bert-base-cased"
   "FacebookAI/roberta-base"
   "google/flan-t5-base"
-  "FacebookAI/roberta-large"
-  "google-bert/bert-large-uncased"
+  # "FacebookAI/roberta-large"
+  # "google-bert/bert-large-uncased"
 )
 seeds=(5 10 15 20 42)
 
 # Loop through each model and seed
 for model in "${models[@]}"; do
   for seed in "${seeds[@]}"; do
-    python main.py --use-gpu --gpus 0 1 2 3 --phase train --batch-size 16 --lr 0.0001 --epochs 70 --model $model --seed $seed --experiment 21_08_deliver
+    python main.py --use-gpu --gpus 0 1 2 3 --phase train --batch-size 16 --lr 0.0001 --epochs 70 --model $model --seed $seed --experiment 21_08_deliver_ver_2
   done
 done
