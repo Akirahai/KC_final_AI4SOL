87a054dc3b7226ae855e0a19f79db451ff3b83b7
Akirahai <lehaivin03@gmail.com>
2024-08-16 12:41:51 +0800

    'epoch'

--- scripts/21_08_fix_again.sh
+++ scripts/21_08_fix_again.sh
@@ -3,13 +3,23 @@
 # Define the models and seeds
 models=(
   "FacebookAI/roberta-large"
-  "google-bert/bert-large-uncased"
 )
-seeds=(5)
+seeds=(5 10 15 20 42)
 
 # Loop through each model and seed
 for model in "${models[@]}"; do
   for seed in "${seeds[@]}"; do
-    python main.py --use-gpu --gpus 0 1 2 3 --phase train --batch-size 16 --lr 0.0001 --epochs 70 --model $model --seed $seed --experiment 21_08_deliver
+    python main.py --use-gpu --gpus 0 1 --phase train --batch-size 16 --lr 0.0001 --epochs 70 --model $model --seed $seed --experiment 21_08_deliver_ver_2
+  done
+done
+
+models=(
+  "google-bert/bert-large-uncased"
+)
+seeds=(5 10 15 20 42)
+  # Loop through each model and seed
+for model in "${models[@]}"; do
+  for seed in "${seeds[@]}"; do
+    python main.py --use-gpu --gpus 2 3 --phase train --batch-size 16 --lr 0.0001 --epochs 70 --model $model --seed $seed --experiment 21_08_deliver_ver_2
   done
 done
--- scripts/21_08_preds_deliver.sh
+++ scripts/21_08_preds_deliver.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Define the models and seeds
-models=(
-  "google-bert/bert-base-cased"
-  "FacebookAI/roberta-base"
-  "google/flan-t5-base"
-  "FacebookAI/roberta-large"
-  "google-bert/bert-large-uncased"
-)
-seeds=(5 10 15 20 42)
+# # Define the models and seeds
+# models=(
+#   "google-bert/bert-base-cased"
+#   "FacebookAI/roberta-base"
+#   "google/flan-t5-base"
+#   "FacebookAI/roberta-large"
+#   "google-bert/bert-large-uncased"
+# )
+# seeds=(5 10 15 20 42)
 
-# Loop through each model and seed
-for model in "${models[@]}"; do
-  for seed in "${seeds[@]}"; do
-    python main.py --use-gpu --gpus 0 1 2 3 --phase test --batch-size 16 --lr 0.0001 --epochs 70 --model "result/21_08_deliver/seed_$seed/$model" --seed $seed --experiment 21_08_deliver --top-k 5
-  done
-done
+# # Loop through each model and seed
+# for model in "${models[@]}"; do
+#   for seed in "${seeds[@]}"; do
+#     python main.py --use-gpu --gpus 0 1 2 3 --phase test --batch-size 16 --lr 0.0001 --epochs 70 --model "result/21_08_deliver/seed_$seed/$model" --seed $seed --experiment 21_08_deliver --top-k 5
+#   done
+# done
