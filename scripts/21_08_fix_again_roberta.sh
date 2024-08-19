b21a106be9f9a38bdd9abd23631c73897c1a37bd
Akirahai <lehaivin03@gmail.com>
2024-08-19 11:44:07 +0800

    'ok'

--- scripts/21_08_fix_again_roberta.sh
+++ scripts/21_08_fix_again_roberta.sh
@@ -9,7 +9,7 @@ seeds=(5 10 15)
 # Loop through each model and seed
 for model in "${models[@]}"; do
   for seed in "${seeds[@]}"; do
-    python main.py --use-gpu --gpus 0 1 2 3 --phase train --batch-size 16 --lr 0.0001 --epochs 70 --model $model --seed $seed --experiment 21_08_deliver_ver_2
+    python main.py --use-gpu --gpus 0 1 2 3 --phase train --batch-size 16 --lr 0.0001 --epochs 70 --model $model --seed $seed --experiment 21_08_deliver_ver_3
   done
 done
 
