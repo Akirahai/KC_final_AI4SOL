e5b3022a54ce71d8d26a768c0f6ad8653500b71a
Akirahai <lehaivin03@gmail.com>
2024-08-20 10:09:26 +0800

    'exp_6'

--- scripts/21_08_exp.sh
+++ scripts/21_08_exp.sh
@@ -2,20 +2,22 @@
 
 # Define the models and seeds
 models=(
-  # "google-bert/bert-base-cased"
+  "google-bert/bert-base-cased"
   # "FacebookAI/roberta-base"
   # "google/flan-t5-base"
-  # "google-bert/bert-large-cased"
   "FacebookAI/roberta-large"
 )
-# seeds=(12 24 42 84 168)
-seeds=(84 168)
+seeds=(12 24 42 84 168)
+gpus=(2 3 2 3 2)
 
 # Loop through each model and seed
 for model in "${models[@]}"; do
-  for seed in "${seeds[@]}"; do
-    python main.py --use-gpu --gpus 0 --phase train --batch-size 16 --lr 0.00001 --epochs 70 --model $model --seed $seed --experiment 21_08_deliver_ver_4
-    sleep 100
+  for i in "${!seeds[@]}"; do
+    seed=${seeds[i]}
+    gpu=${gpus[i]}
 
+    python main.py --use-gpu --gpus $gpu --phase train --batch-size 16 --lr 0.00001 --epochs 70 --model $model --seed $seed --experiment 21_08_deliver_ver_6
+    find 21_08_deliver_ver_6 -type d -name "checkpoint-*" -exec rm -r {} +
+    
   done
 done
--- scripts/21_08_exp_6.sh
+++ scripts/21_08_exp_6.sh
@@ -2,18 +2,22 @@
 
 # Define the models and seeds
 models=(
-  "google-bert/bert-base-cased"
+  # "google-bert/bert-base-cased"
   "FacebookAI/roberta-base"
   "google/flan-t5-base"
-  "FacebookAI/roberta-large"
+  # "FacebookAI/roberta-large"
 )
 seeds=(12 24 42 84 168)
+gpus=(0 1 0 1 0)
 
 # Loop through each model and seed
 for model in "${models[@]}"; do
-  for seed in "${seeds[@]}"; do
-    python main.py --use-gpu --gpus 0 --phase train --batch-size 16 --lr 0.00001 --epochs 70 --model $model --seed $seed --experiment 21_08_deliver_ver_6
-    sleep 600
+  for i in "${!seeds[@]}"; do
+    seed=${seeds[i]}
+    gpu=${gpus[i]}
 
+    python main.py --use-gpu --gpus $gpu --phase train --batch-size 16 --lr 0.00001 --epochs 70 --model $model --seed $seed --experiment 21_08_deliver_ver_6
+    find 21_08_deliver_ver_6 -type d -name "checkpoint-*" -exec rm -r {} +
+    
   done
 done
