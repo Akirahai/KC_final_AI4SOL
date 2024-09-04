12a17ae3b52fcbc8eedb955aef82f59c96ea40aa
Akirahai <lehaivin03@gmail.com>
2024-09-04 10:36:07 +0800

    '04_09_test'

--- scripts/04_09_preds_deliver.sh
+++ scripts/04_09_preds_deliver.sh
@@ -13,7 +13,7 @@ seeds=(12 24 42 84 168)
 # Loop through each model and seed
 for model in "${models[@]}"; do
   for seed in "${seeds[@]}"; do
-    python main.py --use-gpu --gpus 1 2 3 --phase test --batch-size 16 --lr 0.0001 --epochs 70 --model "result/21_08_deliver/seed_$seed/$model" \
+    python main.py --use-gpu --gpus 1 2 3 --phase test --batch-size 16 --lr 0.0001 --epochs 70 --model "result/28_08_deliver/seed_$seed/$model" \
     --seed $seed --experiment 04_09_deliver_train_set_12345 --top-k 5 --training-folder Training_data_12345 --num-of-labels 50 --testing-set ASDiv-600-6th_test.csv
   done
 done
